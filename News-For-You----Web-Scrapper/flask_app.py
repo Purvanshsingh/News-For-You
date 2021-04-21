e1dcc5d806211b868ce0315a0e5a15413ecf7699
Purvansh Singh <purvanshsingh@gmail.com>
2021-04-22 02:28:28 +0530

    all bug fixed.

--- News-For-You----Web-Scrapper/flask_app.py
+++ News-For-You----Web-Scrapper/flask_app.py
@@ -1,18 +1,12 @@
-# doing necessary imports
-
-from flask import Flask, render_template, request,jsonify
-# from flask_cors import CORS,cross_origin
-import requests
+from flask import Flask, render_template, request
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 
-app = Flask(__name__)  # initialising the flask app with the name 'app'
-
-
-
+app = Flask(__name__)
 
 @app.route('/',methods=['POST','GET']) # route with allowed methods as POST and GET
 def index():
+    review = []
     if request.method == 'POST':
         searchString = request.form['content'].replace(" ","") # obtaining the search string entered in the form
         url = 'https://www.indiatvnews.com/topic/' + searchString
@@ -23,6 +17,7 @@ def index():
         NEWS = html.find("ul", {"class": "newsListfull"})
         all_news = NEWS.find_all("li")
         all_news = all_news[:5]
+        review.append(all_news)
 
         url = 'https://www.indiatoday.in/topic/' + searchString +"/ALL"
         Uclient = uReq(url)
@@ -32,19 +27,22 @@ def index():
         List = NEWS.find_all("li")
         List_News = [news.text for news in List if news.text != ""]
         List_News = List_News[:5]
+        review.append(List_News)
 
         url = 'https://www.bbc.co.uk/search?q=' + searchString
         Uclient = uReq(url)
         Data = Uclient.read()
         html = bs(Data, "html.parser")
-        News = html.find("ul", {"class": "css-1vg3h2e-Stack e1y4nx260"})
-        '''L = News.find_all("li")
-        l = [str(i.a.text + i.find_all("p")[1].text) for i in L]
-        l = l[:5]'''
-        '''for i in L:
-            print(i.a.text)
-            text = i.find_all("p")
-            print(text[1].text)'''
+        News = html.find("ul", {"class": "ssrcss-1v7bxtk-StyledContainer enjd40x0"})
+        if News:
+            L = News.find_all("li")
+            l = [str(i.a.text + i.find_all("p")[1].text) for i in L]
+            l = l[:5]
+            review.append(l)
+            for i in L:
+                print(i.a.text)
+                text = i.find_all("p")
+                print(text[1].text)
 
         url = 'https://indianexpress.com/?s=Rajasthan'
         Uclient = uReq(url)
@@ -52,8 +50,9 @@ def index():
         html = bs(Data, "html.parser")
         x = html.find("div", {"class": "search-result"})
         details = x.find_all("div", {"class": "details"})[:5]
+        review.append(details)
 
-        review = [all_news,List_News,l,details,searchString]
+        #review = [all_news,List_News,l,details,searchString]
         return render_template('results.html', reviews=review) # showing the review to the user
     else:
         return render_template('index.html')
